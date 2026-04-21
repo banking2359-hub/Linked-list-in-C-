360646058b6f6e7805909fb490f9b41834b75ba9
Ban <banking2359@gmail.com>
2026-04-21 17:44:07 +0300

    deleting the list in front

--- simple_linked_list.cpp
+++ simple_linked_list.cpp
@@ -5,21 +5,43 @@ struct Node
 {
     int data;
     Node *next;
+    Node *prev;
 };
 
 void add(Node *&head, int data)
 {
     Node *newNode = new Node;
     newNode->data = data;
+    newNode->next = nullptr;
+
     if (head == nullptr)
     {
         head = newNode;
-        head->next = nullptr;
         return;
     }
-    newNode->next = head;
-    head = newNode;
+
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
 }
+
+void deleteFront(Node *&head)
+{
+    if (head == nullptr)
+    {
+        cout << "Empty List!" << endl;
+        return;
+    }
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+}
+
+
 int main()
 {
     Node *head = nullptr;
@@ -27,9 +49,14 @@ int main()
     add(head, 2);
     add(head, 3);
 
+    cout << "List contents: ";
+
+    deleteFront(head);
     while (head != nullptr)
     {
         cout << head->data << " ";
         head = head->next;
     }
+
+    return 0;
 }
